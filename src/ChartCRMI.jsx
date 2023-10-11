0b732cdeb623241aa8d99f59a259f82f6c4beb0d
Hasan Baydoun <hasan-net2010@hotmail.com>
2023-10-11 14:27:20 +0300

    make the chart run by button click

--- src/ChartCRMI.jsx
+++ src/ChartCRMI.jsx
@@ -3,12 +3,33 @@ import * as echarts from "echarts";
 import { HelloWorldSample } from "./components/HelloWorldSample";
 import "./ui/ChartCRMI.css";
 import { get } from "http";
+import { set } from "bro-fs";
 
-export default function ChartCRMI({ Value, ValueName, buttonAction, data, sort, delay, refreshAction }) {
+export default function ChartCRMI({ Value, ValueName, buttonAction, data, sort, delay, refreshAction, booleanAttr }) {
     const [DynamicWidth, setDynamicWidth] = useState(-1);
     const [DynamicHeight, setDynamicHeight] = useState(-1);
-    const [ChartData, setChartData] = useState([]);
+    // const [ChartData, setChartData] = useState([]);
+    const [allowBuild, setAllowBuild] = useState(false);
+    const [refresh, setRefresh] = useState(false);
     const observerDiv = useRef(null);
+    let chartData = [];
+
+    useEffect(() => {
+        console.info("it's withing the use effect of allow build");
+        if (booleanAttr.status === "available") {
+            console.info("it's not allowed to build");
+            if (booleanAttr.value === true) {
+                console.info("it's allowed to build");
+                fillInData();
+                initateDimensions();
+
+                setTimeout(() => {
+                    buildChart();
+                }, delay);
+                setAllowBuild(true);
+            }
+        }
+    }, [booleanAttr, data]);
 
     const getSymbol = item => {
         const symbols = Object.getOwnPropertySymbols(item);
@@ -16,29 +37,29 @@ export default function ChartCRMI({ Value, ValueName, buttonAction, data, sort, 
         return mxSymbol;
     };
     const getMXValues = attr => {
+        console.info("getMXValues", data);
         const x = data.items.map(item => {
             const mySymbol = getSymbol(item);
             return item[mySymbol].jsonData.attributes[attr].value;
         });
         return x;
     };
-    useEffect(() => {
+    const fillInData = () => {
         if (data.status === "available") {
             const ValuesName = getMXValues("LATETYPE");
             const Values = getMXValues("NOT_LATE");
-
-            setChartData(
-                Values.map((item, index) => {
-                    return {
-                        value: item,
-                        name: ValuesName[index]
-                    };
-                })
-            );
+            chartData = Values.map((item, index) => {
+                return {
+                    value: item,
+                    name: ValuesName[index]
+                };
+            });
+            console.info("this is the chart data", data);
         }
-    }, [data?.status]);
+    };
 
     const buildChart = () => {
+        // console.info("it's building the chart", ChartData);
         var chartDom = document.getElementById("chart-container");
         var myChart = echarts.init(chartDom);
         var option;
@@ -77,7 +98,7 @@ export default function ChartCRMI({ Value, ValueName, buttonAction, data, sort, 
                     labelLine: {
                         show: false
                     },
-                    data: ChartData,
+                    data: chartData,
                     animationType: "transition",
                     animationTypeUpdate: "expansion",
                     animation: true,
@@ -92,34 +113,40 @@ export default function ChartCRMI({ Value, ValueName, buttonAction, data, sort, 
         option && myChart.setOption(option);
     };
 
-    useEffect(() => {
-        if (!observerDiv.current || ChartData.length === 0) return;
-
-        const numDataPoints = ChartData.length;
+    const initateDimensions = () => {
+        // console.info("it's initiating the dimensions and this is the chart data", observerDiv.current);
+        console.info("it's initiating the dimensions and this is the chart data", chartData);
+        if (chartData.length === 0) return;
+        const numDataPoints = chartData.length;
         const DynamicWidthValue = 300 + numDataPoints * 20;
         const DynamicHeightValue = 300 + numDataPoints * 20 + 100;
-
         setDynamicWidth(DynamicWidthValue);
         setDynamicHeight(DynamicHeightValue);
+        console.info("it's initiating the dimensions");
         // if (sort.type === "Integer" || sort.type === "Decimal") {
         //     ChartData.sort((a, b) => {
         //         return a.value - b.value;
         //     });
         // }
-        setTimeout(() => {
-            buildChart();
-        }, delay);
+        // setTimeout(() => {
+        //     buildChart();
+        // }, delay);
         if (refreshAction) {
-            setInterval(() => {
-                refreshAction.execute();
-            }, 30000);
+            // setInterval(() => {
+            //     refreshAction.execute();
+            // }, 30000);
         }
-    }, [ChartData]);
+    };
 
     return (
         <div>
-            {/* <input type="number" value={DynamicWidth} onChange={e => setDynamicWidth(e.target.value)} /> */}
-            <div id="chart-container" style={{ height: DynamicHeight, width: DynamicWidth }} ref={observerDiv}></div>
+            {allowBuild && (
+                <div
+                    id="chart-container"
+                    style={{ height: DynamicHeight, width: DynamicWidth }}
+                    ref={observerDiv}
+                ></div>
+            )}
         </div>
     );
 }
